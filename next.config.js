f40aaa8cea18aba02ff9e81d403c485bb0204139
ritesh7092 <riteshrajtiwari.iiitbh@gmail.com>
2025-06-24 21:16:40 +0530

    Updating next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
   },
-}
+  assetPrefix: '/Product-Listing-App', // for GitHub Pages
+  basePath: '/Product-Listing-App',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
